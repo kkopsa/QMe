6eca939aba8a21e1e7afbb89c78dd0e5ef664d7a
Josh Kennelly <ken09001@byui.edu>
2014-04-03 10:00:18 -0600

    Fixed the stylesheet

--- QueueMeDev/WebContent/JS/loadtiles.js
+++ QueueMeDev/WebContent/JS/loadtiles.js
@@ -6,14 +6,13 @@ function showThumbnails() {
 	var friendName = "";
 	var imagehtml = "";
 	for (var i = 0; i < 6; i++) {
-		videoID = "J---aiyznGQ"; // TODO get first videoID in friends
-									// playlist
+		videoID = "J---aiyznGQ"; // TODO get first videoID in friends playlist
 		friendName = "Kevin Kopsa"; // TODO get friends name based on ranking
 		friendName += "<br /></div>";
 		imagehtml = "<div class=\"tile\">";
 		imagehtml += "<a href=\"/QueueMeDev/watch?v=" + videoID + "\">";
 		imagehtml += "<img src=\"http://img.youtube.com/vi/" + videoID
-				+ "/default.jpg\">";
+				+ "/hqdefault.jpg\">";
 		imagehtml += "</a><br />";
 		document.write(imagehtml);
 		document.write(friendName);
@@ -24,13 +23,10 @@ function otherFriends() {
 	var videoID = "";
 	var friendName = "";
 	for (var i = 0; i < 6; i++) {
-		videoID = "QlwilbVYvUg"; // TODO get first videoID in friends
-									// playlist
-		friendName = "Taylor Skinner"; // TODO get friends name based on random
-										// order facebook returns(for now)
-		document
-				.write("<div class=\"tile\"><img src=\"http://img.youtube.com/vi/"
-						+ videoID + "/default.jpg\"><br />");
+		videoID = "QlwilbVYvUg"; // TODO get first videoID in friends playlist
+		friendName = "Taylor Skinner"; // TODO get friends name based on random order facebook returns(for now)
+		document.write("<div class=\"tile\"><img src=\"http://img.youtube.com/vi/"
+						+ videoID + "/hqdefault.jpg\"><br />");
 		document.write(friendName + "<br /></div>");
 	}
 }
