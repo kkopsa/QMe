38aa64fa5b21a8a87799560d02dba4105ab2450c
kkopsa <kopsak2@gmail.com>
2014-04-08 20:17:59 -0600

    formatting cleaning servlets

--- QueueMeDev/WebContent/JS/sendVideo.js
+++ QueueMeDev/WebContent/JS/sendVideo.js
@@ -1,31 +1,35 @@
 function getTitle(videoId) {
-	var url = 'https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=1&q=' + videoId + '&key=AIzaSyASNhaeu2hjaS8Pckm4SzuNGPQVkMW2N7I&jsoncallback=?';
-	
-	$.getJSON(url)
-		.done(function(json) {
-			var title = json.items[0].snippet.title;
-//			$.each(json.items, function(i, item) {
-//				title = item.snippet.title;
-//				alert(title);
-//			});
-			return title;
-		})
-		.error(function(xhr) {
-			//alert(xhr.responseText);
-		});
-	
+	var url = 'https://www.googleapis.com/youtube/v3/search?type=video&part=snippet&maxResults=1&q='
+			+ videoId
+			+ '&key=AIzaSyASNhaeu2hjaS8Pckm4SzuNGPQVkMW2N7I&jsoncallback=?';
+
+	$.getJSON(url).done(function(json) {
+		var title = json.items[0].snippet.title;
+		// $.each(json.items, function(i, item) {
+		// title = item.snippet.title;
+		// alert(title);
+		// });
+		return title;
+	}).error(function(xhr) {
+		// alert(xhr.responseText);
+	});
+
 	return "";
 }
 
 function sendVideo() {
 	var id = $("#videoId").val();
 	var title = getTitle(videoId);
-	
+
 	var friendList = new Array();
 	$("#friends option:selected").each(function() {
 		friendList.push($(this).val());
-	});	
-	
-	$.post("/QMe/AddVideo", {videoId: id, videoTitle: title, friends: friendList});
-	
+	});
+
+	$.post("/QMe/AddVideo", {
+		videoId : id,
+		videoTitle : title,
+		friends : friendList
+	});
+
 }
